22b9e343b5b763bf6feb46cb744ff0cc2c9a32ae
Dilimulati Diliyaer <dilyarswe@gmail.com>
2024-03-17 16:33:28 -0400

    assignRandomColors

--- src/Experience/Camera.js
+++ src/Experience/Camera.js
@@ -18,7 +18,7 @@ export default class Camera
     setInstance()
     {
         this.instance = new THREE.PerspectiveCamera(35, this.sizes.width / this.sizes.height, 0.1, 100)
-        this.instance.position.set(6, 4, 8)
+        this.instance.position.set(10, 3, 0)
         this.scene.add(this.instance)
     }
 
--- src/Experience/World/WorldMap.js
+++ src/Experience/World/WorldMap.js
@@ -27,15 +27,30 @@ export default class WorldMap
     {
         this.model = this.resource.scene
         this.model.scale.set(-0.1, 0.1, -0.1)
+        this.model.position.set(0, 0, 0.5)
         this.scene.add(this.model)
 
         this.model.traverse((child) =>
         {
             if(child instanceof THREE.Mesh)
             {
-                child.castShadow = true
+                this.assignRandomColors(child)
             }
         })
+    }
+
+    assignRandomColors(_child)
+    {
+        // Clone the shared material and apply a random color
+        const clonedMaterial = _child.material.clone();
+
+        // Generate random RGB values within the range of high saturation colors
+        const minSaturation = 0.6; // Minimum saturation value for high saturation colors
+        const maxSaturation = 1.0; // Maximum saturation value for high saturation colors
+        const randomColor = new THREE.Color().setHSL(Math.random(), Math.random() * (maxSaturation - minSaturation) + minSaturation, 0.5);
+        clonedMaterial.color.set(randomColor);
+        
+        _child.material = clonedMaterial;
     }
 
 }
